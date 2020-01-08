c35d4af165b9e2538b88f130699420be013eeac6
Alexander Jangmo <alexander.jangmo@nabsolutions.se>
2020-01-08 10:00:33 +0100

    Skeleton changes.

--- src/main.c
+++ src/main.c
@@ -1,48 +1,65 @@
 #include <stdio.h>
 
-struct Setting_s
+struct Settings
 {
     int max_width;
     int max_depth;
-    int tray[3][3];
+    int tray[4][6];
 };
-
-typedef struct Setting_s Settings;
+struct Tray
+{
+    int rows;
+    int cols;
+};
+typedef struct Tray Tray;
+typedef struct Settings Settings;
 
 int main() 
 {
-    run_program();
+    int no_of_drinks = 0;
+    Tray tray = {4, 6};
+    run_program(no_of_drinks, tray);
     return 0;
 }
 
-void run_program()
+void run_program(int drinks_ordered, Tray tray)
 {
+    if (drinks_ordered == 0 || drinks_ordered > (tray.rows * tray.cols))
+    {
+        return;
+    }
+    
     printf("\n[*] Running Program\n\n");
     int drinks_poured = 0;    
     Settings s = {100, 100, 6};
     for (int row = 0; row < (int)( sizeof(s.tray) / sizeof(s.tray[0])); row++)
     {
-        for (int col = 0; col < (int)(sizeof(s.tray[row][0]) - 1); col++)
+        for (int col = 0; col < (int)(sizeof(s.tray[row][0])); col++)
         {
             unsigned long x = sizeof(s.tray[row][col]);
-            printf("\n[*] Pouring drink in row %d col %d", row);
-            pour();
+            printf("\n[*] Pouring drink in row %d col %d", row, col);
+            pour_drink();
             drinks_poured += 1;
+            if (drinks_ordered == drinks_poured)
+            {
+                printf("\n[*] Done! Poured %d of %d drinks.", drinks_poured, drinks_ordered);
+            }
+            
             move_col();
         }
         move_row();
     }
-    printf("\n[*] Drinks poured %d\n", drinks_poured);
+    printf("\n[*] Drinks poured %d\n.", drinks_poured);
 }
 
 void move_col() {
-    printf("\n[*] Moving col\n");
+    printf("\n[*] Moving col-\n");
 }
 
 void move_row() {
-    printf("\n\n[*] Moving row");
+    printf("\n\n[*] Moving row.");
 }
 
-void pour() {
-    printf("\n[*] Puring.");
+void pour_drink() {
+    printf("\n[*] Pouring.");
 }
